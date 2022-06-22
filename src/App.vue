42588b343d2e8c12bfe04de69d7a795d302a6ec5
Hilton Meyer <development@hiltonmeyer.com>
2022-06-22 09:13:15 +0300

    [feat](header): extract header into component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
 </script>
 
 <template>
-  <img alt="Karrot" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <TheHeader />
 </template>
 
 <style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Karrot" src="../assets/logo.png" />
+  <h1>Karot</h1>
+  <p>Use carrots not sticks</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
