cac16001643fec59e8b2c4dc94b1bbfbab070227
Hilton Meyer <development@hiltonmeyer.com>
2022-06-23 11:02:01 +0300

    [feat](settings): add settings icone

--- src/components/Attribution.vue
+++ src/components/Attribution.vue
@@ -1,22 +1,43 @@
 <template>
-  <a href="https://www.flaticon.com/free-icons/boho" title="boho icons">Boho icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/camel" title="camel icons">Camel icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/cow" title="cow icons">Cow icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/cat" title="cat icons">Cat icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/penguin" title="penguin icons">Penguin icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/kangaroo" title="kangaroo icons">Kangaroo icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/rhinoceros" title="rhinoceros icons">Rhinoceros icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/rabbit" title="rabbit icons">Rabbit icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/shark" title="shark icons">Shark icons created by smalllikeart - Flaticon</a>
-  <a href="https://www.flaticon.com/free-icons/carrot" title="carrot icons">Carrot icons created by Freepik - Flaticon</a>
+  <a href="https://www.flaticon.com/free-icons/boho" title="boho icons"
+    >Boho icons created by smalllikeart - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/camel" title="camel icons"
+    >Camel icons created by smalllikeart - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/cow" title="cow icons"
+    >Cow icons created by smalllikeart - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/cat" title="cat icons"
+    >Cat icons created by smalllikeart - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/penguin" title="penguin icons"
+    >Penguin icons created by smalllikeart - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/kangaroo" title="kangaroo icons"
+    >Kangaroo icons created by smalllikeart - Flaticon</a
+  >
+  <a
+    href="https://www.flaticon.com/free-icons/rhinoceros"
+    title="rhinoceros icons"
+    >Rhinoceros icons created by smalllikeart - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/rabbit" title="rabbit icons"
+    >Rabbit icons created by smalllikeart - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/shark" title="shark icons"
+    >Shark icons created by smalllikeart - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/carrot" title="carrot icons"
+    >Carrot icons created by Freepik - Flaticon</a
+  >
+  <a href="https://www.flaticon.com/free-icons/hammer" title="hammer icons"
+    >Hammer icons created by smalllikeart - Flaticon</a
+  >
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
