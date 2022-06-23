02fbb8c62a4b8a8213bee6bbab72f7e0ef098435
Hilton Meyer <development@hiltonmeyer.com>
2022-06-23 10:58:39 +0300

    [feat](chore): create chore card

---
+++ src/components/ChoreCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <img class="ml-auto w-8 h-8" alt="Icon" :src="`${chore.icon}`" />
+    <div class="mr-auto my-auto">
+      {{ chore.description }}
+    </div>
+    <div class="flex flex-wrap my-auto">
+      <img
+        v-for="index in chore.value"
+        :key="index"
+        class="w-4 h-4"
+        alt="Done"
+        src="/assets/carrot.png"
+      />
+    </div>
+</template>
+
+<script setup>
+defineProps({ chore: Object });
+</script>
+
+<style>
+
+</style>
--- src/components/ChoresList.vue
+++ src/components/ChoresList.vue
@@ -1,32 +1,17 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useChoresStore } from '../stores/chores';
+import ChoreCard from './ChoreCard.vue';
+
+const { chores } = storeToRefs(useChoresStore());
+</script>
+
 <template>
   <div
     class="grid grid-flow-col grid-cols-3 gap-4 m-4"
     v-for="chore in chores"
     :key="chore.id"
   >
-    <img class="ml-auto w-8 h-8" alt="Icon" :src="`${chore.icon}`" />
-    <div class="mr-auto my-auto">
-      {{ chore.description }}
-    </div>
-    <div class="flex flex-wrap my-auto">
-      <img
-        v-for="index in chore.value"
-        :key="index"
-        class="w-4 h-4"
-        alt="Done"
-        src="/assets/carrot.png"
-      />
-    </div>
+    <chore-card :chore="chore" />
   </div>
 </template>
-
-<script setup>
-import { storeToRefs } from 'pinia';
-import { useChoresStore } from '../stores/chores';
-
-const { chores } = storeToRefs(useChoresStore());
-</script>
-
-<style>
-
-</style>
